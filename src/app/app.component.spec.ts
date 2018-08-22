d7772c4646313f6a557e5aa2208ad592be9e7acf
YI ZHANG <scaccterran@gmail.com>
2018-08-22 18:52:51 -0400

    load rankings from all supported years on application load

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { Component, DebugElement } from '@angular/core';
+import { of } from 'rxjs';
 import { By } from '@angular/platform-browser';
 import { RouterLinkStubDirective } from '../testing/router-link-stub-directive';
+import { RankingLookupService } from './modules/data-providers/rankings-data/ranking-lookup.service';
 import { AppComponent } from './app.component';
 
 // tslint:disable:component-selector
@@ -13,14 +15,23 @@ describe('AppComponent', () => {
     let fixture: ComponentFixture<AppComponent>;
     let linkDebugElements: DebugElement[];
     let routerLinks: RouterLinkStubDirective[];
+    let rankingLookup: jasmine.SpyObj<RankingLookupService>;
+    let getRankingsSpy: jasmine.Spy;
 
     beforeEach(async(() => {
+
+        rankingLookup = jasmine.createSpyObj('RankingLookupService', ['getRankings']);
+        getRankingsSpy = rankingLookup.getRankings.and.returnValue(of(null));
+
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent,
                 RouterLinkStubDirective,
                 RouterOutletStubComponent
             ],
+            providers: [
+                { provide: RankingLookupService, useValue: rankingLookup }
+            ]
         }).compileComponents();
     }));
 
@@ -57,5 +68,13 @@ describe('AppComponent', () => {
 
             expect(routerLinks[i].navigatedTo).toEqual(routerLinks[i].linkParams);
         }
+    });
+
+    it('should load rankings from all supported years on load', () => {
+
+        const startYear = 2013;
+        const endYear = new Date().getFullYear();
+        const totalYears = endYear - startYear + 1;
+        expect(getRankingsSpy.calls.count()).toEqual(totalYears);
     });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RankingLookupService } from './modules/data-providers/rankings-data/ranking-lookup.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    constructor(private rankingLookup: RankingLookupService) { }
+
+    ngOnInit(): void {
+
+        this.loadRankings();
+    }
+
+    private loadRankings(): void {
+
+        const startYear = 2013;
+        const currentYear = new Date().getFullYear();
+
+        for (let i = startYear; i <= currentYear; i++) {
+
+            this.rankingLookup.getRankings(i).subscribe();
+        }
+    }
 }
